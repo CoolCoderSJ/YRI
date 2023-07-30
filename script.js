34415b81c687e4b71fa14b5c752bf31ed2e89b7e
Shuchir Jain <53063247+CoolCoderSJ@users.noreply.github.com>
2023-07-29 23:18:30 -0400

    header bg

---
+++ script.js
@@ -0,0 +1,129 @@
+tailwind.config = {
+    darkMode: 'class',
+    theme: {
+        extend: {
+            colors: {
+                primary: { "50": "#f8fafc", "100": "#f1f5f9", "200": "#e2e8f0", "300": "#cbd5e1", "400": "#94a3b8", "500": "#64748b", "600": "#475569", "700": "#334155", "800": "#1e293b", "900": "#0f172a", "950": "#020617" }
+            }
+        },
+        fontFamily: {
+            'body': [
+                'Inter',
+                'ui-sans-serif',
+                'system-ui',
+                '-apple-system',
+                'system-ui',
+                'Segoe UI',
+                'Roboto',
+                'Helvetica Neue',
+                'Arial',
+                'Noto Sans',
+                'sans-serif',
+                'Apple Color Emoji',
+                'Segoe UI Emoji',
+                'Segoe UI Symbol',
+                'Noto Color Emoji'
+            ],
+            'sans': [
+                'Inter',
+                'ui-sans-serif',
+                'system-ui',
+                '-apple-system',
+                'system-ui',
+                'Segoe UI',
+                'Roboto',
+                'Helvetica Neue',
+                'Arial',
+                'Noto Sans',
+                'sans-serif',
+                'Apple Color Emoji',
+                'Segoe UI Emoji',
+                'Segoe UI Symbol',
+                'Noto Color Emoji'
+            ]
+        }
+    }
+}
+
+window.onload = () => {
+    // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+    if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+        document.getElementsByTagName("html")[0].classList.add('dark');
+    } else {
+        document.getElementsByTagName("html")[0].classList.remove('dark')
+    }
+    var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
+    var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+
+    // Change the icons inside the button based on previous settings
+    if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+        themeToggleLightIcon.classList.remove('hidden');
+    } else {
+        themeToggleDarkIcon.classList.remove('hidden');
+    }
+
+    var themeToggleBtn = document.getElementById('theme-toggle');
+
+    themeToggleBtn.addEventListener('click', function () {
+
+        // toggle icons inside button
+        themeToggleDarkIcon.classList.toggle('hidden');
+        themeToggleLightIcon.classList.toggle('hidden');
+
+        // if set via local storage previously
+        if (localStorage.getItem('color-theme')) {
+            if (localStorage.getItem('color-theme') === 'light') {
+                document.getElementsByTagName("html")[0].classList.add('dark');
+                localStorage.setItem('color-theme', 'dark');
+            } else {
+                document.getElementsByTagName("html")[0].classList.remove('dark');
+                localStorage.setItem('color-theme', 'light');
+            }
+
+            // if NOT set via local storage previously
+        } else {
+            if (document.getElementsByTagName("html")[0].classList.contains('dark')) {
+                document.getElementsByTagName("html")[0].classList.remove('dark');
+                localStorage.setItem('color-theme', 'light');
+            } else {
+                document.getElementsByTagName("html")[0].classList.add('dark');
+                localStorage.setItem('color-theme', 'dark');
+            }
+        }
+
+    });
+
+    const N = 20;
+    const arcsData = [...Array(N).keys()].map(() => ({
+        startLat: (Math.random() - 0.5) * 180,
+        startLng: (Math.random() - 0.5) * 360,
+        endLat: (Math.random() - 0.5) * 180,
+        endLng: (Math.random() - 0.5) * 360,
+        color: [['red', 'white', 'blue', 'green'][Math.round(Math.random() * 3)], ['red', 'white', 'blue', 'green'][Math.round(Math.random() * 3)]]
+    }));
+
+    Globe()
+        .globeImageUrl('//unpkg.com/three-globe/example/img/earth-night.jpg')
+        .arcsData(arcsData)
+        .arcColor('color')
+        .arcDashLength(() => Math.random())
+        .arcDashGap(() => Math.random())
+        .arcDashAnimateTime(() => Math.random() * 4000 + 500)
+        .backgroundColor("#00000000")
+        (document.getElementById('globeViz'))
+
+}
+
+var rotation = 0, 
+    scrollLoc = $(document).scrollTop();
+$(window).scroll(function() {
+    var newLoc = $(document).scrollTop();
+    var diff = scrollLoc - newLoc;
+    rotation += diff, scrollLoc = newLoc;
+    var rotationStr = "rotate(" + rotation + "deg)";
+    $("#bg_style").html(`.bg_item {
+        -webkit-transform: ${rotationStr};
+        -moz-transform: ${rotationStr};
+        transform: ${rotationStr}
+    }`);
+});
