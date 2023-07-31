e96f84af705a5fada9dcd03f40493ee41be11ee4
Shuchir Jain <53063247+CoolCoderSJ@users.noreply.github.com>
2023-07-31 10:36:14 -0400

    bg gradient

--- script.js
+++ script.js
@@ -113,17 +113,3 @@ window.onload = () => {
         (document.getElementById('globeViz'))
 
 }
-
-var rotation = 0, 
-    scrollLoc = $(document).scrollTop();
-$(window).scroll(function() {
-    var newLoc = $(document).scrollTop();
-    var diff = scrollLoc - newLoc;
-    rotation += diff, scrollLoc = newLoc;
-    var rotationStr = "rotate(" + rotation + "deg)";
-    $("#bg_style").html(`.bg_item {
-        -webkit-transform: ${rotationStr};
-        -moz-transform: ${rotationStr};
-        transform: ${rotationStr}
-    }`);
-});
